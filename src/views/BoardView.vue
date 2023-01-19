3747783faa00f59458d3660f160090c9e7286181
xmatuskaa <95504128+xmatuskaa@users.noreply.github.com>
2023-01-19 21:43:52 +0100

    board with pieces

--- src/views/BoardView.vue
+++ src/views/BoardView.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="board">
         <div class="WSquare">
-            <v-img src="../assets/Brook.png"></v-img>
+            <img src="../assets/Brook.png"/>
         </div>
-        <div class="BSquare"></div>
+        <div class="BSquare">
+            <img src="../assets/Bbishop.png"/>
+        </div>
         <div class="WSquare"></div>
         <div class="BSquare"></div>
         <div class="WSquare"></div>
@@ -11,14 +13,15 @@
         <div class="WSquare"></div>
         <div class="BSquare"></div>
 
-        <div class="BSquare"></div>
-        <div class="WSquare"></div>
-        <div class="BSquare"></div>
-        <div class="WSquare"></div>
-        <div class="BSquare"></div>
-        <div class="WSquare"></div>
-        <div class="BSquare"></div>
-        <div class="WSquare"></div>
+        <div class="BSquare">
+            <img src="../assets/Bpawn.png"/></div>
+        <div class="WSquare"><img src="../assets/Bpawn.png"/></div>
+        <div class="BSquare"><img src="../assets/Bpawn.png"/></div>
+        <div class="WSquare"><img src="../assets/Bpawn.png"/></div>
+        <div class="BSquare"><img src="../assets/Bpawn.png"/></div>
+        <div class="WSquare"><img src="../assets/Bpawn.png"/></div>
+        <div class="BSquare"><img src="../assets/Bpawn.png"/></div>
+        <div class="WSquare"><img src="../assets/Bpawn.png"/></div>
 
         <div class="WSquare"></div>
         <div class="BSquare"></div>
@@ -69,8 +72,12 @@
         <div class="BSquare"></div>
         <div class="WSquare"></div>
         <div class="BSquare"></div>
-        <div class="WSquare"></div>
-        <div class="BSquare"></div>
+        <div class="WSquare">
+            <img src="../assets/Wqueen.png"/>
+        </div>
+        <div class="BSquare">
+            <img src="../assets/Wking.png"/>
+        </div>
         <div class="WSquare"></div>
         <div class="BSquare"></div>
         <div class="WSquare"></div>
@@ -82,27 +89,40 @@ export default{
 }
 </script>
 <style scoped>
-.WSquare{
-background-color: beige;
-}
-.BSquare{
-background-color: rgb(86, 120, 86);
-}
-.board{
-    width: min(0.85*100vh, 0.85*100vw);
-    height: min(0.85*100vh, 0.85*100vw);
-    margin-left:auto;
-    margin-right: auto;
-    border: 5px solid;
-    display: grid;
-    grid-template-columns: repeat(8,1fr);
-}
-.WSquare img {
-    flex-shrink: 0;
-    width: 100%;
-}
-.BSquare img {
-    width: 100%;
-    height: 100%;
-}
+    .WSquare{
+        background-color: beige;
+        aspect-ratio: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .BSquare{
+        background-color: rgb(86, 120, 86);
+        aspect-ratio: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .board{
+        width: min(0.85*100vh, 0.85*100vw);
+        height: min(0.85*100vh, 0.85*100vw);
+        margin-left:auto;
+        margin-right: auto;
+        border: 5px solid;
+        display: grid;
+        grid-template-columns: repeat(8,1fr);
+    }
+    .WSquare img {
+        object-fit:cover;
+        max-width: 100%;
+        max-height: auto;
+        aspect-ratio: 1;
+    }
+    .BSquare img {
+        object-fit:cover;
+        max-width: 100%;
+        max-height: auto;
+        aspect-ratio: 1;
+        
+    }
 </style>
